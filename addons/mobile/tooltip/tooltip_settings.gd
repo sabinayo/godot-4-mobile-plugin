2ad52d75a7239948c6980005ceef9bcb0dbf8e69
Sabinayo <sabinayohiembo@gmail.com>
2026-03-20 16:04:45 +0000

    Fix tooltip setting get_data() returning helpbar data

--- addons/mobile/tooltip/tooltip_settings.gd
+++ addons/mobile/tooltip/tooltip_settings.gd
@@ -1,90 +1,92 @@
 @tool
-extends VBoxContainer;
+extends VBoxContainer
 
-signal setting_changed(which: String);
+signal setting_changed(which: String)
 
-const FAST_SETTINGS_SCENE: GDScript = preload("../tools/fast_settings_editor.gd");
+const FAST_SETTINGS_SCENE: GDScript = preload("../tools/fast_settings_editor.gd")
 
-var _obj_path: String;
-var _signals_manager_path: String;
+var _obj_path: String
+var _signals_manager_path: String
 var _props := {
 	Mobile.Tooltip.MetaProperties.DISPLAY: {
-		"updating": false, "node": "HBox/enable",
+		"updating": false,
+		"node": "HBox/enable",
 		"value": "button_pressed",
 	},
-};
+}
 
-var settings: EditorResourcePicker;
+var _tooltip_settings: EditorResourcePicker
 
 
 func _ready() -> void:
-	if not Engine.is_editor_hint(): return;
+	if not Engine.is_editor_hint():
+		return
 	
-	settings = FAST_SETTINGS_SCENE.new("MobileTooltipFastSettings");
-	$VBox/HBox.add_child(settings);
+	_tooltip_settings = FAST_SETTINGS_SCENE.new("MobileTooltipFastSettings")
+	%FastSettingsContainer.add_child(_tooltip_settings)
 	
-	settings.resource_changed.connect(
+	_tooltip_settings.resource_changed.connect(
 		func (resource: Resource) -> void:
-			%custom_settings_editor_enabler.disabled = resource == null;
-			%custom_settings_editor_enabler.set_pressed_no_signal(resource != null);
-	, CONNECT_DEFERRED);
+			%custom_settings_editor_enabler.disabled = resource == null
+			%custom_settings_editor_enabler.set_pressed_no_signal(resource != null)
+	, CONNECT_DEFERRED)
 
 
 func set_fast_settings_data(data: Dictionary) -> void:
-	settings.set_data(data);
-	_obj_path = data["edited_object"];
-	_signals_manager_path = data["signals_manager"];
+	_tooltip_settings.set_data(data)
+	_obj_path = data["edited_object"]
+	_signals_manager_path = data["signals_manager"]
 
 
 func _on_enable_toggled(button_pressed: bool) -> void:
-	var prop: String = Mobile.Tooltip.MetaProperties.DISPLAY;
+	var prop: String = Mobile.Tooltip.MetaProperties.DISPLAY
 	
 	if !_props[prop]["updating"]:
-		setting_changed.emit(prop, button_pressed);
+		setting_changed.emit(prop, button_pressed)
 	
-	$VBox.visible = button_pressed;
+	%AvailableSettings.visible = button_pressed
 
 
 func _on_custom_settings_editor_enabler_pressed() -> void:
-	var _signals_manager = get_node(_signals_manager_path);
-	var meta_data: Dictionary = _signals_manager.get_meta("tooltip_overrides");
+	var _signals_manager = get_node(_signals_manager_path)
+	var meta_data: Dictionary = _signals_manager.get_meta("tooltip_overrides")
 	
 #	if err != OK:
-#		fast_settings.edited_resource = null;
-#		%custom_settings_editor_enabler.disabled = true;
-#		return;
+#		fast_settings.edited_resource = null
+#		%custom_settings_editor_enabler.disabled = true
+#		return
 	
 	if meta_data.has(_obj_path):
-		meta_data.erase(_obj_path);
-		_signals_manager.set_meta("", meta_data);
+		meta_data.erase(_obj_path)
+		_signals_manager.set_meta("", meta_data)
 	
-	settings.edited_resource = null;
-	%custom_settings_editor_enabler.disabled = true;
+	_tooltip_settings.edited_resource = null
+	%custom_settings_editor_enabler.disabled = true
 
 
 func disable_setting(setting: String) -> void:
-	get_node(_props[setting]["node"]).get_parent().hide();
+	get_node(_props[setting]["node"]).get_parent().hide()
 
 
 func get_setting(setting: String) -> Variant:
-	return get_node(_props[setting]["node"]).get(_props[setting]["value"]);
+	return get_node(_props[setting]["node"]).get(_props[setting]["value"])
 
 
 func update_setting(setting: String, value: Variant) -> void:
-	if not _props.has(setting): return;
+	if not _props.has(setting):
+		return
 	
-	_props[setting]["updating"] = true;
-	var node = get_node(_props[setting]["node"]);
-	node.set_indexed(_props[setting]["value"], value);
-	_props[setting]["updating"] = false;
+	_props[setting]["updating"] = true
+	var node = get_node(_props[setting]["node"])
+	node.set_indexed(_props[setting]["value"], value)
+	_props[setting]["updating"] = false
 
 
 func get_data() -> Dictionary:
-	var data := {};
-	var settings: Array = Mobile.HelpBar.MetaProperties.values();
+	var data := {}
+	var tooltip_settings: Array = Mobile.Tooltip.MetaProperties.values()
 	
-	for setting in settings:
-		data[setting] = get_setting(setting);
+	for setting in tooltip_settings:
+		data[setting] = get_setting(setting)
 	
-	return data;
-
+	return data
